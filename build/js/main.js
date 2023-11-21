f9a2503603c893865684ba265fd4aced0cc821b7
StoreFusionMain <tdur@protonmail.com>
2023-11-21 17:15:50 +0530

    [changes] dave grey ts tut untill capter 4(finished) functions, continue with the tutorial and also mainly read the fucking docs bruv typescri[tlang.org

--- build/js/main.js
+++ build/js/main.js
@@ -50,5 +50,72 @@ var Grade;
     Grade[Grade["B"] = 4] = "B";
     Grade[Grade["A"] = 5] = "A";
 })(Grade || (Grade = {}));
-console.log("a");
 console.log(Grade.B);
+// whats the differece between type and interface
+// we cannot do this: type postID = stringOrNumber; 
+// think about interfaces like objects or classes
+// and type area alias or as any kind of typescript type that might assign
+// Literal types: assigning a type that you can customize
+let userName;
+userName = 'DailySh';
+// DRY = Dont Repeat Yourself
+// functions:
+const add = (a, b) => {
+    return a + b;
+};
+const checkFunc = (a) => {
+    console.log(a);
+};
+const logMsg = (message) => {
+    console.log(message);
+};
+checkFunc(100);
+// interface mathFunction {
+//     (a: number, b: number): number 
+// }
+let multiply = function (c, d) {
+    return c * d;
+};
+logMsg(multiply(20, 21));
+// optional parameters
+const addAll = (a, b, c) => {
+    if (c !== undefined) {
+        return a + b + c;
+    }
+    return a + b;
+};
+//using default param values below:
+const sumAll = (a, b, c = 2) => {
+    return a + b + c;
+};
+logMsg(sumAll(1, 2));
+let arr = [1, 2, 3, 4, 50];
+logMsg(arr.reduce((prev, curr) => prev + curr)); // understand this line as well <---
+const total = (nums) => {
+    return nums.reduce((prev, curr) => prev + curr);
+};
+console.log("after using the ... operator // or the rest paraneter for the array that is being passed into the function");
+logMsg(total(arr));
+const createError = (errMsg) => {
+    throw new Error(errMsg);
+};
+// 
+const infinite = () => {
+    let i = 1;
+    while (true) {
+        i++;
+        if (i > 100)
+            break;
+    }
+};
+const numberOrString = (a) => {
+    console.log(typeof a);
+};
+const isNumber = (a) => {
+    return typeof a === 'number'
+        ? true : false;
+};
+numberOrString("asd");
+// type assertions and type casting
+// its basically you the coding using telling the typescript compiler that you know more about the types, 
+// so it needs to listen to you.
--- src/main.ts
+++ src/main.ts
@@ -66,15 +66,122 @@ console.log(greetGuitarist(JP));
 // unlike most Typescript features, enums are not a type-level addition to js but added to the language and runtime.
 
 
-enum Grade {
+enum Grade { // it is default enumerated starting from 0 and goes on from there +1
     U = 1,
     D,
     C,
     B,
     A,
 }
-
-console.log("a");
 console.log(Grade.B);
 
- 
+// lesson 4
+
+// type alliases, 
+// explaination: we are representing our typescript types witha different name or alias
+// we 
+type stringOrNumber = string | number
+
+type stringOrNumberArray = (string | number)[] 
+
+type postID = stringOrNumber; 
+
+// whats the differece between type and interface
+// we cannot do this: type postID = stringOrNumber; 
+
+// think about interfaces like objects or classes
+// and type area alias or as any kind of typescript type that might assign
+
+// Literal types: assigning a type that you can customize
+
+
+let userName: 'DailySh' | 'torch';
+
+userName = 'DailySh'; 
+
+// DRY = Dont Repeat Yourself
+
+// functions:
+
+const add = (a: number, b: number): number => {
+    return a + b;
+}
+
+const checkFunc = (a: stringOrNumber) => {
+    console.log(a);
+}
+
+const logMsg = (message: any): void => {
+    console.log(message);
+}
+
+checkFunc(100);
+
+// function types 
+
+type mathFunction = (a: number, b: number) => number;
+// interface mathFunction {
+//     (a: number, b: number): number 
+// }
+
+
+let multiply: mathFunction = function(c, d){
+    return c * d;
+}
+
+logMsg(multiply(20,21));
+
+// optional parameters
+
+const addAll = (a: number, b: number, c?: number ): number => { // optional parameters must come before the required ones. it needs to be the last in the list
+    if(c!== undefined) {
+        return a + b + c;
+    }
+    return a + b;
+}
+
+//using default param values below:
+const sumAll = (a: number, b: number, c: number = 2  ): number => { 
+    return a + b + c;
+}
+
+logMsg(sumAll(1, 2));
+
+let arr = [1,2,3,4,50];
+logMsg(arr.reduce((prev,curr) => prev + curr)); // understand this line as well <---
+
+const total = (nums: number[]): number => {
+    return nums.reduce((prev,curr) => prev + curr)
+}
+console.log("after using the ... operator // or the rest paraneter for the array that is being passed into the function");
+logMsg(total(arr));
+
+
+const createError = (errMsg: string): never => {
+    throw new Error(errMsg);
+}
+
+// 
+const infinite = () => {
+    let i: number = 1;
+    while(true) {
+        i++;
+        if(i>100) break;
+    }
+}
+
+const numberOrString = (a: number | string): any => {
+    console.log(typeof a);
+}
+
+const isNumber = (a: any): boolean => {
+    return typeof a === 'number'
+        ? true : false;
+}
+
+numberOrString("asd");
+
+// type assertions and type casting
+// its basically you the coding using telling the typescript compiler that you know more about the types, 
+// so it needs to listen to you.
+
